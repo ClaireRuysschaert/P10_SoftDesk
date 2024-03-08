c0eedf7d6faa8a965168ca884cfa37d502906963
ClaireRuysschaert <netclaire@hotmail.fr>
2024-03-08 17:10:45 +0100

    feat: Add Project model on projects app

--- softdesk/projects/models.py
+++ softdesk/projects/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+from softdesk.accounts.models import SoftUser, Contributor
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(
+        SoftUser, on_delete=models.CASCADE, related_name="authored_projects"
+    )
+    type = models.CharField(
+        max_length=3,
+        choices=[
+            ("BAE", "back-end"),
+            ("FRE", "front-end"),
+            ("IOS", "iOs"),
+            ("AND", "Android"),
+        ],
+    )
+    contributors = models.ManyToManyField(
+        SoftUser, through=Contributor, related_name="contributed_projects"
+    )
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        have_not_been_created = not self.pk
+        super().save(*args, **kwargs)
+        if have_not_been_created:
+            self.contributors.add(self.author)

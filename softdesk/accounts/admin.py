7bce2da5d7bf6033f2c7b6457660ecb809c9efef
ClaireRuysschaert <netclaire@hotmail.fr>
2024-03-15 10:41:31 +0100

    feat: Add contributor and projects admin

--- softdesk/accounts/admin.py
+++ softdesk/accounts/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import SoftUser
+from .models import SoftUser, Contributor
 
 @admin.register(SoftUser)
 class SoftUserAdmin(admin.ModelAdmin):
@@ -8,3 +8,9 @@ class SoftUserAdmin(admin.ModelAdmin):
     list_display = ('username', 'email', 'birthdate', 'updated_on', 'can_be_contacted', 'can_be_shared')
     search_fields = ('username', 'email')
     list_filter = ('can_be_contacted', 'can_be_shared')
+
+@admin.register(Contributor)
+class ContributorAdmin(admin.ModelAdmin):
+    model = Contributor
+    list_display = ('user', 'project', 'date_joined')
+    search_fields = ('user', 'project')
--- softdesk/projects/admin.py
+++ softdesk/projects/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Project
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    model = Project
+    list_display = ('name', 'description', 'created_on', 'updated_on', 'author', 'type')
+    search_fields = ('name', 'author', 'type')

44ec0ab39b31f0dc7f5f38884916ddf5497e52e8
ClaireRuysschaert <netclaire@hotmail.fr>
2024-03-21 14:29:30 +0100

    Add django-filters to the project

--- softdesk/settings.py
+++ softdesk/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "softdesk.projects",
     "softdesk.accounts",
     "rest_framework",
+    "django_filters",
 ]
 
 REST_FRAMEWORK = {
@@ -48,6 +49,7 @@ REST_FRAMEWORK = {
     ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
 }
 
 AUTH_USER_MODEL = "accounts.SoftUser"
